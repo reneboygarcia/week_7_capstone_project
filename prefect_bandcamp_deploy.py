54760a7731818d227f985b7b726a9e725fcbe627
reneboygarcia <reneboygarcia@mgmail.com>
2023-03-24 11:47:10 +0800

    amend code

--- prefect_bandcamp_deploy.py
+++ prefect_bandcamp_deploy.py
@@ -9,7 +9,7 @@ github_block = GitHub.load("bandcamp-github-block")
 
 # https://docs.prefect.io/api-ref/prefect/deployments/#prefect.deployments.Deployment.build_from_flow
 gcs_git_dep = Deployment.build_from_flow(
-    flow=etl_parent_web_gcs,
+    flow="etl-parent-web-gcs",
     name="bandcamp-flow",
     storage=github_block,
 )
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     gcs_git_dep.apply()
 
 # to deploy
-# prefect deployment run etl_parent_web_gcs/bandcamp-flow
+# prefect deployment run el_parent_web_gcs/bandcamp-flow
 #  --params '{"years":[2019, 2020], "months": [4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 3, 1]}'
